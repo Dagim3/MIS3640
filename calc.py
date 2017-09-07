c98b1b29f515b15d83575bca957829c529830e42
Dagim3 <31517245+Dagim3@users.noreply.github.com>
2017-09-06 20:41:40 -0400

    calc.py

---
+++ calc.py
@@ -0,0 +1,23 @@
+print('Excercise 1 & 2')
+print('Hello World')
+print(1+1)
+print((42*60)+42)
+print(10/1.61)
+print((10/1.61)/((42*60)+42))
+
+print('Excercise 1')
+print((4/3)*3.14*(5*5*5))
+print (((24.95*60)+3+(.75*59))) 
+print ((((8.15*1)+(7.12*3)+(8.15*1)+652)/100)+.4)
+print = (89-82)
+
+
+print = ('Practice')
+first_name = 'Dagim'
+last_name = 'Girma'
+fullname = first_name +" "+ last_name
+print = fullname
+print = ('Today is %2d-%02d.' % (9, 5))
+print = ('Pi equals %.2f.' % 3.1415926)
+print = ('Age: %s. Gender: %s' % (20, "Male"))
+print = ('Growth rate: %d %%' % 7)
