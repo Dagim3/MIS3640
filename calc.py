988460636eafeff35f2e537b2c99eb78a7ba868f
Dagim3 <31517245+Dagim3@users.noreply.github.com>
2017-09-07 23:56:34 -0400

    calc.py

--- calc.py
+++ calc.py
@@ -1,23 +1,51 @@
-print('Excercise 1 & 2')
+print('August 31- Excercise 1 & 2')
 print('Hello World')
 print(1+1)
 print((42*60)+42)
 print(10/1.61)
 print((10/1.61)/((42*60)+42))
 
-print('Excercise 1')
+print('September 5 - Excercise 1')
 print((4/3)*3.14*(5*5*5))
 print (((24.95*60)+3+(.75*59))) 
 print ((((8.15*1)+(7.12*3)+(8.15*1)+652)/100)+.4)
-print = (89-82)
+print (89-82)
 
+print('September 7 - Excercise 1,2,3')
+a = 3
+print (a + 2)
+b = .5
+print (b)
+c = 3
+c == 5.0
+print (c)
+d = 10
+e = d > 9
+print (e)
+print (5/2 == 5/2.0)
+print (3.0 - 1.0 != 5.0 - 3.0)
+print (3 > 4 or (2 < 3 and 9 > 10))
+print (4 > 5 or 3 < 4 and 9 > 8)
+print (not(4 > 3 and 100 > 6))
+import time
+print (time.time())
+1437746094.5735958
 
-print = ('Practice')
+print ('Practice')
 first_name = 'Dagim'
 last_name = 'Girma'
 fullname = first_name +" "+ last_name
-print = fullname
-print = ('Today is %2d-%02d.' % (9, 5))
-print = ('Pi equals %.2f.' % 3.1415926)
-print = ('Age: %s. Gender: %s' % (20, "Male"))
-print = ('Growth rate: %d %%' % 7)
+print (fullname)
+print ('Today is %2d-%02d.' % (9, 5))
+print ('Pi equals %.2f.' % 3.1415926)
+print ('Age: %s. Gender: %s' % (20, "Male"))
+print ('Growth rate: %d %%' % 7)
+print ('3.14e-2 # Scientific notation')
+print ('3.14e-2 # Scientific notation')
+print ('123 + 222 # Integer addition')
+print ('1.5 * 4 # Floating-point multiplication')
+print ('2 ** 100 # 2 to the power 100')
+print ('len(str(2 ** 1000000)) # How many digits in a really BIG number?')
+import math
+print(math.pi)
+print(math.sqrt(85))
